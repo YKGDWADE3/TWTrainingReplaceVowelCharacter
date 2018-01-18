668ada36d672a3b9762dcfbbca2982bafe97b20f
YKGDWADE3 <superwadeg3@gmail.com>
2018-01-18 14:44:28 +0800

    refactor

--- src/com/tw/practice/StringProcess.java
+++ src/com/tw/practice/StringProcess.java
@@ -4,16 +4,14 @@ public class StringProcess {
 
     private  final String REPLACE = "mommy";
 
+    /**
+     * 字符串元音替换（占比30%以上时）
+     */
     public String replaceVowel(String string){
-        int vowelNumber = 0;
-        for (int i = 0; i < string.length(); i++) {
-            char temp = string.charAt(i);
-            if (temp == 'a' || temp == 'e' || temp == 'i' || temp == 'o' || temp == 'u'){
-                vowelNumber++;
-            }
-        }
+        int vowelNumber = getVowelNumbers(string);
+
         if (vowelNumber * 10 > string.length() * 3){
-            boolean beforeVowel = false;//上一个字符是否是元音
+            boolean beforeVowel = false;//上一个字符是否是元音，用来处理连续元音的情况
             StringBuffer resultBuffer = new StringBuffer();
             for (int i = 0; i < string.length(); i++) {
                 char temp = string.charAt(i);
@@ -32,5 +30,16 @@ public class StringProcess {
         }else{
             return string;
         }
+    }
+
+    private int getVowelNumbers(String string) {
+        int result = 0;
+        for (int i = 0; i < string.length(); i++) {
+            char temp = string.charAt(i);
+            if (temp == 'a' || temp == 'e' || temp == 'i' || temp == 'o' || temp == 'u'){
+                result++;
+            }
+        }
+        return result;
     }
 }
